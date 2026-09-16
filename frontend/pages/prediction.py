b4f2f7bd2757e168ad9f4ea913d86972d89396b5
Kun Bi <kunbi3456@gmail.com>
2026-09-16 15:38:54 -0400

    finished docker compose

--- frontend/pages/prediction.py
+++ frontend/pages/prediction.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 import pandas as pd
 
-api_url="http://127.0.0.1:8000/predict"
+api_url="http://api:8000/predict"
 Demo_text="""
     California State Teachers Retirement System reduced its 
     Crocs stake by 2.9%, selling 1,722 shares and retaining 
